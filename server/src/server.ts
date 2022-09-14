fcb507adb7924d03e5d6142beed3eb96129b4154
Carlos Marx <carlosmarx27@gmail.com>
2022-09-14 11:27:12 -0300

    nitial routes on server app

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,30 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 
-app.get('/ads', (request, response) => {
+app.get('/games', (req, res) => {
+  return res.json([]);
+});
+
+
+app.post('/ads', (req, res) => {
+  return res.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+  // const gameId = request.params.id;
+  // return response.json(gameId);
   return response.json([
     {id: 1, name: "Anúncio 1"},
     {id: 2, name: "Anúncio 2"},
     {id: 3, name: "Anúncio 3"},
     {id: 4, name: "Anúncio 4"},
   ])
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+  const adId = request.params.id;
+  return response.json([adId]);
 })
 
 app.listen(3333)
