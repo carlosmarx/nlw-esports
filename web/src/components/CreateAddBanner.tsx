f60f783cdefe79e241a498b01a97815e7d21799d
Carlos Marx <carlosmarx27@gmail.com>
2022-09-15 16:58:22 -0300

    List games from server start modal on web

---
+++ web/src/components/CreateAddBanner.tsx
@@ -0,0 +1,24 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { MagnifyingGlassPlus } from "phosphor-react";
+
+export function CreateAddBanner() {
+    return (
+        <>
+            <div className='pt-1 bg-nlw-gradient self-stretch mt-8 overflow-hidden rounded-lg'> 
+                <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
+                <div>
+                    <strong className='text-white text-2xl font-black block'>Não encontrou seu duo? </strong>
+                    <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players! </span>
+                </div>
+
+                <Dialog.Trigger>
+                    <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 transition-colors text-white rounded flex items-center gap-3'>
+                        <MagnifyingGlassPlus size={24}/>
+                        Publicar anúncio
+                    </button>
+                </Dialog.Trigger>
+                </div>
+            </div>
+        </>
+    )
+}
